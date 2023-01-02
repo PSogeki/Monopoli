b03a48679aaf9b7c505953da51c1c619bc00813a
PSogeki <paolodibiase1@gmail.com>
2023-01-02 22:02:18 +0100

    modifica creazione jar eseguibile

--- src/main/java/it/unimol/monopoli/gui/GamePanel.java
+++ src/main/java/it/unimol/monopoli/gui/GamePanel.java
@@ -39,7 +39,7 @@ public class GamePanel extends JPanel {
         this.setLayout(null);
 
         try {
-            bgImage = ImageIO.read(new File("src\\main\\java\\it\\unimol\\monopoli\\images\\sfondo800.jpg"));
+            bgImage = ImageIO.read(new File("images\\sfondo800.jpg"));
         } catch (IOException e) {
             JOptionPane.showMessageDialog(
                 null, 
